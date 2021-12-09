2e3beb72738dee2e4319dfc4378b9abf3a5fc14a
cynamon jules <julescweb@mail.com>
2021-12-09 15:26:13 +0100

    gerer les donees

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,65 @@
-const clefAPI = "ee4d0408076eb196a91997289aeefb6f";
-let resultatsAPI;
+    const clefAPI = "ee4d0408076eb196a91997289aeefb6f";
+    let resultatsAPI;
+    const temps = document.querySelector(".temps");
+    const temperature = document.querySelector(".temperature");
+    const localisation = document.querySelector(".localisation");
+    const heure = document.querySelectorAll(".heure-nom-previson");
+    const tempPourH = document.querySelectorAll(".heure-prevision-valeur");
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      // console.log(position);
-      let long = position.coords.longitude;
-      let lat = position.coords.latitude;
-      appelAPI(long, lat);
-    },
-    () => {
-      alert(
-        `vous avez refuser la géolocalisation, l'application ne peut pas fonctionner. Veuillez l'activer ! `
-      );
-    }
-  );
-}
+            if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                // console.log(position);
+                let long = position.coords.longitude;
+                let lat = position.coords.latitude;
+                appelAPI(long, lat);
+                },
+                () => {
+                alert(
+                    `Vous avez refusé la géolocalisation, l'application ne peur pas fonctionner, veuillez l'activer.!`
+                    );
+                });
+            }
 
-function appelAPI(long, lat) {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${clefAPI}`
-  )
-    .then((reponse) => {
-      return reponse.json();
-    })
+        function appelAPI(long, lat) {
+        fetch(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${clefAPI}`
+        )
+            .then((reponse) => {
+            return reponse.json();
+            })
 
-    .then((data) => {
-      console.log(data);
-    });
-}
+            .then((data) => {
+                
+                // console.log(data);
+                resultatsAPI = data;
+
+                temps.innerText = resultatsAPI.current.weather[0].description;
+                temperature.innerText = `${Math.trunc(resultatsAPI.current.temp)}°`;
+                localisation.innerText = resultatsAPI.timezone;
+
+                // les heures par tranches de trois avec leurs temperatures
+
+                    let heureActuelle = new Date().getHours();
+
+                    for (let i = 0; i < heure.length; i++) {
+                    let heureIncr = heureActuelle + i * 3;
+
+                    if (heureIncr > 24) {
+                        heure[i].innerText = `${heureIncr - 24} h`;
+                    } else if (heureIncr === 24) {
+                        heure[i].innerText = "00 h";
+                    } else {
+                        heure[i].innerText = `${heureIncr} h`;
+                    }
+                    }
+
+                // temps pour 3 heure
+
+                for (let j = 0; j < tempPourH.length; j++ ){
+                    tempPourH[j].innerText = `${Math.trunc(
+                      resultatsAPI.hourly[j * 3].temp)}°`;
+                }
+
+            });
+        }
